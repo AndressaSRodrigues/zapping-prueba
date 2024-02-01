4392b877f489ee59eb22fbb2565c8bf497f8ae6f
AndressaSRodrigues <srandressa2@gmail.com>
2024-02-01 15:26:20 -0300

    Added success component

--- zapping-prueba/src/App.vue
+++ zapping-prueba/src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
 // import Checkout from './components/Checkout.vue';
-import Loader from './components/Loader.vue';
+// import Loader from './components/Loader.vue';
+import Success from './components/Success.vue';
 </script>
 
 <template>
-    <Loader />
+    <Success />
 </template>
 
 <style lang="scss" scoped>
---
+++ zapping-prueba/src/components/Success.vue
@@ -0,0 +1,81 @@
+<script setup>
+import zappingBrand from '../assets/zapping2.png';
+import successCheck from '../assets/success.png';
+</script>
+
+<template>
+    <nav>
+        <img :src="zappingBrand" alt="Logo de la marca Zapping" width="auto" />
+        <button>Ayuda</button>
+    </nav>
+    <section>
+        <img :src="successCheck" alt="Success checkmark" width="auto">
+        <p class="success-title">Muy bien!</p>
+        <p class="success-message">Tu suscripción a Zapping está lista, ya puedes comenzar a ver TV.</p>
+        <button>Regresar</button>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+* {
+    font-family: $font-family-poppins;
+}
+
+nav {
+    @include flex-row;
+    padding: 3rem 1.5rem 1.5rem 1.5rem;
+
+    img {
+        height: 1rem;
+    }
+
+    button {
+        @include nav-button;
+    }
+}
+
+section {
+    @include flex-col;
+    text-align: center;
+
+    img {
+        width: 8rem;
+        margin: 8rem 0 4rem 0;
+        opacity: 0;
+        animation: fadein 2s forwards;
+    }
+
+    @keyframes fadein {
+        0% {
+            opacity: 0;
+        }
+
+        100% {
+            opacity: 1;
+        }
+    }
+
+    .success-title {
+        font-size: 1.6rem;
+        font-weight: 600;
+    }
+
+    .success-message {
+        font-size: 0.9rem;
+        font-weight: 400;
+        color: $gray;
+    }
+
+    button {
+        width: 21.5rem;
+        height: 3.25rem;
+        margin: 2rem 0;
+        color: $white;
+        font-weight: 700;
+        font-size: 14px;
+        border: 1px solid $gray;
+        border-radius: 4px;
+        background-color: $bgColor;
+    }
+}
+</style>
